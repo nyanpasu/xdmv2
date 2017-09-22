155f39d417c70d3270f0dceea874a51de1f5618e
nyanpasu <nekomune@comfy.moe>
2017-09-23 02:44:19 +0800

    Remove test that scaled output to 50% size

--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -1,7 +1,7 @@
 #version 330 core
-const vec2 quadVertices[4] = vec2[4]( vec2(-1.0, -1.0), vec2(1.0, -1.0), vec2(-1.0, 1.0), vec2(1.0, 1.0) );
+uniform vec2 quadVertices[4] = vec2[4]( vec2(-1.0, -1.0), vec2(1.0, -1.0), vec2(-1.0, 1.0), vec2(1.0, 1.0) );
 void main()
 {
   vec2 vert = quadVertices[gl_VertexID];
-  gl_Position = vec4(vert.x / 2.0, vert.y / 2.0, 0.0, 1.0);
+  gl_Position = vec4(vert, 0.0, 1.0);
 }
